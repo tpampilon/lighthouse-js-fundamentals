84a400cda37e2ee9b582e5795f2a3aae4f4037b0
TedP <ted.pampilon@gmail.com>
2021-02-12 19:27:51 +0000

    Array exercise with for loops and while loops

---
+++ ingredients.js
@@ -0,0 +1,24 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log(" ");
+
+for (let i = 0; i < ingredients.length; i++) {
+	console.log(ingredients[i]);
+}
+
+console.log(" ");
+
+let y = 0;
+while (y < ingredients.length) {
+console.log(ingredients[y]);
+y++
+}
+
+console.log(" ");
+
+for (let x = (ingredients.length - 1); x >= 0; x--) {
+  console.log(ingredients[x]);
+}
+
+
+
+
